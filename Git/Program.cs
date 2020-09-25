6fc5f170b9d9458a4ee4838b892d8f02cd83e33f
Vicente6186 <vicentechemin@gmail.com>
2020-09-25 16:17:42 -0300

    design novo!

--- Git/Program.cs
+++ Git/Program.cs
@@ -6,10 +6,15 @@ namespace Git
     {
         static void Main(string[] args)
         {
+            Console.Write("---------");
             Console.WriteLine("Hello World!");
+            Console.Write("---------");
             Console.WriteLine("Salvo");
+            Console.Write("---------");
             Console.WriteLine("Finalizando projeto!");
+            Console.Write("---------");
             Console.WriteLine("Bom dia novamente!");
+            Console.Write("---------");
             Console.WriteLine("Mais um!");
         }
     }
